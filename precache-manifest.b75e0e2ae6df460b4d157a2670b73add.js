5a83bfc571d0a8e01e2fc7f298ff3f116bc49508
ritikvashistha <ritik@sutratechlabs.com>
2020-12-02 20:48:11 +0530

    Updates

---
+++ precache-manifest.b75e0e2ae6df460b4d157a2670b73add.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f773aa083cf3c4a67447ff64edd3af45",
+    "url": "/reactstore/index.html"
+  },
+  {
+    "revision": "8c7733e7cee83a2cab19",
+    "url": "/reactstore/static/css/main.92b2bd58.chunk.css"
+  },
+  {
+    "revision": "5bc982f142fae390f439",
+    "url": "/reactstore/static/js/2.0bb29c1b.chunk.js"
+  },
+  {
+    "revision": "280b48dbc3281f342da16f66e41cf46c",
+    "url": "/reactstore/static/js/2.0bb29c1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c7733e7cee83a2cab19",
+    "url": "/reactstore/static/js/main.1f5991c3.chunk.js"
+  },
+  {
+    "revision": "2372f48ab0932cc9ac28",
+    "url": "/reactstore/static/js/runtime-main.39e87aed.js"
+  },
+  {
+    "revision": "8894b7d37bd41e50b31318e97d50b323",
+    "url": "/reactstore/static/media/loading.8894b7d3.gif"
+  },
+  {
+    "revision": "7ac3ba3ab8503352edfc941d7ecd430d",
+    "url": "/reactstore/static/media/logo.7ac3ba3a.svg"
+  },
+  {
+    "revision": "016d4fcc158e863ea29afc6b6ff6c3ff",
+    "url": "/reactstore/static/media/mainBcg.016d4fcc.jpeg"
+  }
+]);
