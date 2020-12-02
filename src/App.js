58c7e8d934377f14b11727649c8be32508b1db5d
ritikvashistha <ritik@sutratechlabs.com>
2020-12-02 21:16:45 +0530

    hash router added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 // react router dom
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 // pages
 import About from "./pages/About";
 import Cart from "./pages/Cart";
